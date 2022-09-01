d77dc43cdb8a0d606aa8398668d0915bdfd2b7dc
icanbe444 <icanbe444@gmail.com>
2022-09-01 22:16:13 +0100

    Added Validation for fields

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -1,25 +1,41 @@
+import email
+from turtle import title
 from requests import request
 from rest_framework import serializers
 from attr import fields
 from django import forms
 from .models import Product
 from rest_framework.reverse import reverse
-
+from . import validators
 
 class ProductSerializer(serializers.ModelSerializer):
     my_discount = serializers.SerializerMethodField(read_only=True) #this is used to introduce a name that isnt in the model
+    
+    title = serializers.CharField(validators=[ validators.unique_product_title, validators.validate_title_no_hello])
+    name = serializers.CharField(source='title', read_only=True)
     class Meta:
         model= Product
         fields = [
             'url',
             'id',
+            # 'email',
             'title',
+            'name',
             'content',
             'price',
             'sale_price',
             'my_discount',
 
         ]
+    # #validating field of my choice. This will make sure two products do not have ths same title
+    # def validate_title(self, value):
+    #     qs = Product.objects.filter(title__iexact= value) #iexact looks for exact name and case sensitive
+    #     if qs.exists():
+    #         raise serializers.ValidationError(f"{value} is already a product name")
+    #     return value
+
+
+
 
     def get_url(self, obj):
         # return f"/api/products/{obj.pk}"
@@ -29,7 +45,13 @@ class ProductSerializer(serializers.ModelSerializer):
 
         return reverse("product-detail", kwargs= {"pk": obj.pk}, request=request)
 
+    # def create(self, validated_data):
+    #     email = validated_data.pop('email')
+    #     obj = super().create(validated_data)
+    #     return obj
 
+
+        
     def get_my_discount(self, obj):
         if not hasattr(obj , 'id'):
             return None
---
+++ backend/products/validators.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from .models import Product
+from rest_framework.validators import UniqueValidator
+
+
+
+
+#validating field of my choice. This will make sure two products do not have ths same title
+def validate_title(value):
+    qs = Product.objects.filter(title__iexact= value) #iexact looks for exact name and case sensitive
+    if qs.exists():
+        raise serializers.ValidationError(f"{value} is already a product name")
+    return value
+
+
+def validate_title_no_hello(value):
+    if "hello" in value.lower():
+        raise serializers.ValidationError(f"Hello is not allowed" )
+        
+unique_product_title = UniqueValidator(queryset=Product.objects.all())
